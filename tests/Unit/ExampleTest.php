4b88766554aa25307196ee679f2c0d078e770f1c
whitebear0909 <a.hayama@crowdechain.com>
2022-06-27 12:56:00 +0900

    feature: unit test for CommentController

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Http\Controllers\CommentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ExampleTest extends TestCase
 {
@@ -13,6 +16,18 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $this->assertTrue(true);
+        $commentController = new CommentController();
+
+        $request = Request::create('/comments', 'POST',[
+            'message'     =>  'test message',
+            'author'  =>  'test author',
+            'parent_comment_id' => null
+        ]);
+
+        $createCommentRes = $commentController->postComment($request);
+        $this->assertEquals(200, $createCommentRes->getStatusCode());
+        
+        $getCommentsRes = $commentController->index();
+        $this->assertEquals(200, $getCommentsRes->getStatusCode());
     }
 }
